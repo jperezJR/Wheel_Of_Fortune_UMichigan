80826b1868e67129f0b7501a67b0fbf26f8d80ae
perezjr <joseperezjunior@gmail.com>
2020-10-11 22:43:51 -0300

    WOFComputerPlayer class definition (part C)

--- WheelOfFortune.py
+++ WheelOfFortune.py
@@ -1,3 +1,5 @@
+import random
+
 VOWEL_COST = 250
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 VOWELS = 'AEIOU'
@@ -11,7 +13,7 @@ class WOFPlayer():
         self.prizes = []
     
     def addMoney(self, amt):
-        self.prizeMoney = amt
+        self.prizeMoney += amt
         
     def goBankrupt(self):
         self.prizeMoney = 0
@@ -29,16 +31,68 @@ class WOFHumanPlayer(WOFPlayer):
 
     def getMove(self, category, obscuredPhrase, guessed):
         prompt = """
-        {} has ${}\n
-        \n
+        {} has ${}
+        
         Category: {}
         Phrase: {}
         Guessed: {}
-        \n
+        
         Guess a letter, phrase, or type 'exit' or 'pass':""".format(self.name, self.prizeMoney, category, obscuredPhrase, guessed)
         
         userinp = input(prompt)
         return userinp
 
 
+
+# Write the WOFComputerPlayer class definition (part C) here
+class WOFComputerPlayer(WOFPlayer):
+
+    SORTED_FREQUENCIES = 'ZQXJKVBPYGFWMUCLDRHSNIOATE'
+
+    def __init__(self, name, difficulty):
+        WOFPlayer.__init__(self, name)
+        self.difficulty = difficulty
+
+    def smartCoinFlip(self):
+        rand_num = random.randint(1, 10)
+
+        if rand_num > self.difficulty:
+            return True
+        elif rand_num <= self.difficulty:
+            return False
+
+    def getPossibleLetters(self, guessed):
+        poss_letter = []
+        for letter in LETTERS:
+            if letter not in guessed:
+                poss_letter.append(letter)
+            if self.prizeMoney < VOWEL_COST:
+                    for p_letter in poss_letter:
+                        if p_letter in VOWELS:
+                            poss_letter.remove(p_letter)
+        return poss_letter
+
+
+
+    def getMove(self, category, obscuredPhrase, guessed):
+
+        move = self.getPossibleLetters(guessed)
+        if not move:
+            return 'pass'
+        
+        else:
+            if self.smartCoinFlip() == True:
+                for freq_letter in self.SORTED_FREQUENCIES[::-1]:
+                    if freq_letter in move:
+                        return freq_letter
+            else:
+                return random.choice(move)
+            
+
+
+
+        
+
+
+    
 
